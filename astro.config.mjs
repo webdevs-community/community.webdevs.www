4cb8ed6177e7d59e89e6176d6afacab1753f9a1c
David Hellmann <david@hellmann.io>
2023-03-07 22:38:11 +0100

    chore: typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,12 @@
+// https://astro.build/config
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
-// import vercel from "@astrojs/vercel/serverless";
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 // import image from "@astrojs/image";
 
-// https://astro.build/config
+// import vercel from "@astrojs/vercel/serverless";
+import cloudflare from '@astrojs/cloudflare';
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_SITE,
   trailingSlash: "never",
