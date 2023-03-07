2528e113556f86fdc34245bf2c3bf2b3c8660c46
David Hellmann <david@hellmann.io>
2023-03-07 23:40:10 +0100

    ➕ add: critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // https://astro.build/config
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import svelte from "@astrojs/svelte";
 // import image from "@astrojs/image";
 
@@ -7,6 +7,8 @@ import vercel from "@astrojs/vercel/serverless";
 // import cloudflare from '@astrojs/cloudflare';
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import critters from "astro-critters";
+
 export default defineConfig({
   site: process.env.PUBLIC_SITE,
   trailingSlash: "never",
@@ -15,10 +17,14 @@ export default defineConfig({
   build: {
     format: "file"
   },
-  integrations: [svelte(), mdx(), tailwind({
-    config: {
-      path: "./tailwind.config.cjs",
-      applyBaseStyles: false
-    }
-  })]
+  integrations: [
+    svelte(),
+    mdx(),
+    tailwind({
+      config: {
+        path: "./tailwind.config.cjs",
+        applyBaseStyles: false
+      }
+    }),
+    critters()]
 });
